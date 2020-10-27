f821fc14cbe6f6f706437ceb5b15ae626386fdf5
ochi-tama <per.raid@gmail.com>
2020-10-28 01:50:38 +0900

    section3

--- src/money.test.ts
+++ src/money.test.ts
@@ -9,3 +9,8 @@ test("5 * 3 equals to 15", () => {
   const money = new Money(5);
   expect(money.times(3).amount).toBe(15);
 });
+
+test("test  equality", () => {
+  expect(new Money(5)).toStrictEqual(new Money(5));
+  expect(new Money(5)).not.toStrictEqual(new Money(6));
+});
--- task.md
+++ task.md
@@ -5,3 +5,5 @@
 - [ ] amount を private にする
 - [x] Dollar の副作用をどうする
 - [ ] Money の丸め処理をどうする
+- [x] equals
+- [x] hashCode
