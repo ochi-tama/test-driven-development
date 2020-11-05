7a244a5ded0db065c0f853596af2720627344093
ochi-tama <per.raid@gmail.com>
2020-11-05 23:45:40 +0900

    section4

--- src/money.test.ts
+++ src/money.test.ts
@@ -1,13 +1,15 @@
+import Dollar from "./money";
 import Money from "./money";
 
 test("5 * 2 equals to 10", () => {
+  // 意図を伝える
   const money = new Money(5);
-  expect(money.times(2).amount).toBe(10);
+  expect(money.times(2)).toStrictEqual(new Dollar(10));
 });
 
 test("5 * 3 equals to 15", () => {
   const money = new Money(5);
-  expect(money.times(3).amount).toBe(15);
+  expect(money.times(3)).toStrictEqual(new Dollar(15));
 });
 
 test("test  equality", () => {
