0ad03b17ac5076d84ef0b65656a73b06733615bd
ochi-tama <per.raid@gmail.com>
2020-11-08 20:47:39 +0900

    不用なファイルを削除

--- src/dollar.ts
+++
@@ -1,8 +0,0 @@
-import Money from "./money";
-class Dollar extends Money {
-  times(mult: number): Money {
-    return new Dollar(this.amount * mult);
-  }
-}
-
-export default Dollar;
--- src/franc.ts
+++
@@ -1,8 +0,0 @@
-import Money from "./money";
-class Franc extends Money {
-  times(mult: number): Money {
-    return new Franc(this.amount * mult);
-  }
-}
-
-export default Franc;
