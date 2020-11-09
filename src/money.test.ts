1fb1295306a83bf81f96c6503d47915bd4562cb7
ochi-tama <49814684+ochi-tama@users.noreply.github.com>
2020-11-09 23:20:04 +0900

    Section10 (#5)
    * timesメソッドを共通化
    
    * タスクの追加
    
    * timesメソッドを親クラスに引き上げ

--- src/money.test.ts
+++ src/money.test.ts
@@ -1,15 +1,15 @@
 /* eslint-disable no-magic-numbers */
-import Money from "./money";
+import Money, { Franc } from "./money";
 
 test("5 * 2 equals to 10", () => {
   // 意図を伝える
   const money = Money.dollar(5);
-  expect(money.times(2)).toStrictEqual(Money.dollar(10));
+  expect(money.times(2)).toEqual(Money.dollar(10));
 });
 
 test("5 * 3 equals to 15", () => {
   const money = Money.dollar(5);
-  expect(money.times(3)).toStrictEqual(Money.dollar(15));
+  expect(money.times(3)).toEqual(Money.dollar(15));
 });
 
 test("Dollar equality", () => {
@@ -24,7 +24,7 @@ test("Franc equality", () => {
 
 test("5CHF * 2 equals to 10 CHF", () => {
   const money = Money.franc(5);
-  expect(money.times(2)).toStrictEqual(Money.franc(10));
+  expect(money.times(2)).toEqual(Money.franc(10));
 });
 
 test("5CHF not equals to 5 dollar", () => {
@@ -34,4 +34,8 @@ test("5CHF not equals to 5 dollar", () => {
 test("currency equals", () => {
   expect("USD").toBe(Money.dollar(1).currency);
   expect("CHF").toBe(Money.franc(1).currency);
+});
+
+test("test diffrenct class euality", () => {
+  expect(new Money(1, "CHF")).toEqual(new Franc(1, "CHF"));
 });
